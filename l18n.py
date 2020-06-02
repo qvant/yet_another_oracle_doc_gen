768e87fe124e2f1d2bc1d631cfaae19b6d504f7a
Andrew Kuskov <qvant86@gmail.com>
2020-06-02 20:56:25 +0300

    Added geting english message when no msg in original file

--- l18n.py
+++ l18n.py
@@ -2,6 +2,7 @@ import json
 import codecs
 from messages import M_TRUE, M_FALSE
 
+DEFAULT_LOCALE = 'english'
 
 class L18n:
     # Class for translation messages to chosen language
@@ -9,6 +10,7 @@ class L18n:
         self.locale = ''
         self.encoding = None
         self.msg_map = []
+        self.alternative = None
 
     def set_encoding(self, encoding):
         self.encoding = encoding
@@ -18,9 +20,17 @@ class L18n:
         f = "l18n\\" + name + ".lng"
         fp = codecs.open(f, 'r', "utf-8")
         self.msg_map = json.load(fp)
+        if self.locale != DEFAULT_LOCALE:
+            self.alternative = L18n()
+            self.alternative.set_locale(DEFAULT_LOCALE)
 
     def get_message(self, msg_type):
-        msg = self.msg_map[msg_type]
+        if msg_type in self.msg_map.keys():
+            msg = self.msg_map[msg_type]
+        elif self.locale != DEFAULT_LOCALE:
+            msg = self.alternative.get_message(msg_type)
+        else:
+            raise KeyError("Can't find message {} in locale {} (default locale {})".format(msg_type, self.locale, DEFAULT_LOCALE))
         if self.encoding is not None:
             msg = str(msg.encode(self.encoding))
         return msg
